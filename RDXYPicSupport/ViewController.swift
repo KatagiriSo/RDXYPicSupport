52c6d8ccd24320ad5c139f6baeb7f7108cfb87f9
katagiri <so@katagiri>
2016-11-10 13:58:32 +0900

    Initial Commit

---
+++ RDXYPicSupport/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RDXYPicSupport
+//
+//  Created by 片桐奏羽 on 2016/11/10.
+//  Copyright © 2016年 Rodhos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
